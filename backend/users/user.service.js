46206a7a5a37f49ffa2bd8b2e98c6d3d4217fb22
Pham Anh Nguyen Hong <tunpanh@gmail.com>
2021-01-15 01:44:41 +0700

    - Initial project - Login - Register - Share button

---
+++ backend/users/user.service.js
@@ -0,0 +1,48 @@
+﻿const config = require('config.json');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const db = require('_helpers/db');
+const User = db.User;
+
+module.exports = {
+    authenticate,
+    getById,
+    create,
+};
+
+async function authenticate({ username, password }) {
+    const user = await User.findOne({ username });
+
+    if (user) {
+        if(bcrypt.compareSync(password, user.hash)) {
+            const token = jwt.sign({ sub: user.id }, config.secret, { expiresIn: '7d' });
+            return {
+                ...user.toJSON(),
+                token
+            };
+        } else {
+            return { message: 'Username or password is incorrect' };//{"user": "a", "pass": "b"};//res.status(400).json({ message: 'Username or password is incorrect' })
+        }
+    }
+}
+
+async function getById(id) {
+    return await User.findById(id);
+}
+
+async function create(userParam) {
+    // validate
+    if (await User.findOne({ username: userParam.username })) {
+        throw 'Username "' + userParam.username + '" is already taken';
+    }
+
+    const user = new User(userParam);
+
+    // hash password
+    if (userParam.password) {
+        user.hash = bcrypt.hashSync(userParam.password, 10);
+    }
+
+    // save user
+    await user.save();
+}
---
+++ backend/users/users.controller.js
@@ -0,0 +1,44 @@
+﻿const express = require('express');
+const router = express.Router();
+const userService = require('./user.service');
+
+// routes
+router.post('/authenticate', authenticate);
+router.get('/current', getCurrent);
+router.get('/:id', getById);
+
+module.exports = router;
+
+function authenticate(req, res, next) {
+    userService.authenticate(req.body)
+        .then((user) => {
+            if (user) {
+                if (user.message) {
+                    return res.status(400).json({ message: 'Password is incorrect' });
+                } else {
+                    return res.json(user);
+                }
+            }
+            else
+                return register(req, res, next);
+        })
+        .catch(err => next(err));
+}
+
+function register(req, res, next) {
+    userService.create(req.body)
+        .then(() => authenticate(req, res, next))
+        .catch(err => next(err));
+}
+
+function getCurrent(req, res, next) {
+    userService.getById(req.user.sub)
+        .then(user => user ? res.json(user) : res.sendStatus(404))
+        .catch(err => next(err));
+}
+
+function getById(req, res, next) {
+    userService.getById(req.params.id)
+        .then(user => user ? res.json(user) : res.sendStatus(404))
+        .catch(err => next(err));
+}
