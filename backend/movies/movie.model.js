caa00c4918c8f54abde4e88952409f600c3bcf5e
Pham Anh Nguyen Hong <tunpanh@gmail.com>
2021-01-18 03:58:19 +0700

    Get info of shared youtube url by google api

--- backend/movies/movie.model.js
+++ backend/movies/movie.model.js
@@ -4,6 +4,15 @@ const Schema = mongoose.Schema;
 const schema = new Schema({
     sharedBy: { type: String, required: true },
     youtubeUrl: { type: String, unique: true, required: true },
+    thumbnailUrl: { type: String },
+    title: { type: String },
+    description: { type: String },
+    likeCount: { type: String },
+    dislikeCount: { type: String },
+});
+
+schema.virtual('youtubeId').get(function() {
+    return this.youtubeUrl.split('?')[1].split('&')[0].split('=')[1];
 });
 
 schema.set('toJSON', {
--- backend/movies/movie.service.js
+++ backend/movies/movie.service.js
@@ -1,5 +1,6 @@
 ﻿const config = require('config.json');
 const db = require('_helpers/db');
+const https = require('https');
 const Movie = db.Movie;
 
 module.exports = {
@@ -13,6 +14,33 @@ async function getAll() {
 }
 
 async function create(userParam) {
-    const movie = new Movie(userParam);
+    let movie = new Movie(userParam);
+    const info = await getMovieInfo(movie.youtubeId);
+    movie.thumbnailUrl = info.items[0].snippet.thumbnails.high.url;
+    movie.title = info.items[0].snippet.title;
+    movie.description = info.items[0].snippet.description;
+    movie.likeCount = info.items[0].statistics.likeCount;
+    movie.dislikeCount = info.items[0].statistics.dislikeCount;
+
     await movie.save();
+}
+
+function getMovieInfo(youtubeId) {
+    return new Promise((resolve, reject) => {
+        const req = https.get(`https://www.googleapis.com/youtube/v3/videos?id=${youtubeId}&key=${config.youtubeApiKey}&part=snippet,statistics`, (res) => {
+            res.setEncoding('utf8');
+            let responseBody = '';
+
+            res.on('data', (chunk) => {
+                responseBody += chunk;
+            });
+
+            res.on('end', () => {
+                resolve(JSON.parse(responseBody));
+            });
+        }).on('error', (e) => {
+            reject(e);
+        });
+        req.end();
+    });
 }
